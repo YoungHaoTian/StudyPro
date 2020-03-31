42659f85ddb531c2ed309c3379d6d3de05348aa9
Mr.Young <no.bugs@foxmail.com>
2020-03-31 12:31:54 +0800

    2020.03.31：系统搭建

---
+++ src/main/java/com/cdut/studypro/services/PeopleService.java
@@ -0,0 +1,16 @@
+package com.cdut.studypro.services;
+
+import com.cdut.studypro.beans.People;
+
+import java.util.List;
+
+/**
+ * @description:
+ * @author: Mr.Young
+ * @date: 2020-03-31 12:19
+ * @email: no.bugs@foxmail.com
+ * @qq: 1023704092
+ */
+public interface PeopleService {
+    List<People> queryAll();
+}
---
+++ src/main/webapp/WEB-INF/views/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  User: Mr.Young
+  Date: 2020/3/31
+  Time: 11:12
+  Email: no.bugs@foxmail.com
+  Description: 
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+Index Page!
+</body>
+</html>
